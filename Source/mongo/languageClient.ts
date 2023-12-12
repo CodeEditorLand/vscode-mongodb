6734d3a57aac83919cf59fffee512460a3c1d0fe
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-13 01:16:11 +0200

    squash!

--- Source/mongo/languageClient.ts
+++ Source/mongo/languageClient.ts
@@ -2,79 +2,47 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import * as path from "path";
-import * as nls from "vscode-nls";
-import {
-	workspace,
-	languages,
-	ExtensionContext,
-	extensions,
-	Uri,
-	Range,
-} from "vscode";
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	RequestType,
-	ServerOptions,
-	TransportKind,
-	NotificationType,
-} from "vscode-languageclient";
-import { Database } from "./mongo";
+import * as path from 'path';
+import * as nls from 'vscode-nls';
+import { workspace, languages, ExtensionContext, extensions, Uri, Range } from 'vscode';
+import { LanguageClient, LanguageClientOptions, RequestType, ServerOptions, TransportKind, NotificationType } from 'vscode-languageclient';
+import { Database } from './mongo';
 
 const localize = nls.loadMessageBundle();
 
 namespace ConnectDBRequest {
-	export const type: RequestType<string, string, any, any> = new RequestType(
-		"vscode/content"
-	);
+	export const type: RequestType<string, string, any, any> = new RequestType('vscode/content');
 }
 
 namespace ColorSymbolRequest {
-	export const type: RequestType<string, Range[], any, any> = new RequestType(
-		"json/colorSymbols"
-	);
+	export const type: RequestType<string, Range[], any, any> = new RequestType('json/colorSymbols');
 }
 
 export default class MongoDBLanguageClient {
+
 	private client: LanguageClient;
 
 	constructor(context: ExtensionContext) {
 		// The server is implemented in node
-		let serverModule = context.asAbsolutePath(
-			path.join("out", "src", "mongo", "languageServer.js")
-		);
+		let serverModule = context.asAbsolutePath(path.join('out', 'src', 'mongo', 'languageServer.js'));
 		// The debug options for the server
-		let debugOptions = { execArgv: ["--nolazy", "--debug=6005"] };
+		let debugOptions = { execArgv: ['--nolazy', '--debug=6005'] };
 
 		// If the extension is launch in debug mode the debug server options are use
 		// Otherwise the run options are used
 		let serverOptions: ServerOptions = {
-			run: {
-				module: serverModule,
-				transport: TransportKind.ipc,
-				options: debugOptions,
-			},
-			debug: {
-				module: serverModule,
-				transport: TransportKind.ipc,
-				options: debugOptions,
-			},
+			run: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
+			debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
 		};
 
 		// Options to control the language client
 		let clientOptions: LanguageClientOptions = {
 			// Register the server for mongo javascript documents
-			documentSelector: ["mongo"],
+			documentSelector: ['mongo'],
 		};
 
 		// Create the language client and start the client.
-		this.client = new LanguageClient(
-			"mongo",
-			localize("mongo.server.name", "Mongo Language Server"),
-			serverOptions,
-			clientOptions
-		);
+		this.client = new LanguageClient('mongo', localize('mongo.server.name', 'Mongo Language Server'), serverOptions, clientOptions);
 		let disposable = this.client.start();
 
 		// Push the disposable to the context's subscriptions so that the
@@ -84,11 +52,11 @@ export default class MongoDBLanguageClient {
 
 	connect(database: Database): void {
 		const uri = Uri.parse(database.server.id);
-		const connectionString = `${uri.scheme}://${uri.authority}/${database.id}?${uri.query}`;
-		this.client.sendRequest("connect", { connectionString });
+		const connectionString = `${uri.scheme}://${uri.authority}/${database.id}?${uri.query}`
+		this.client.sendRequest('connect', { connectionString });
 	}
 
 	disconnect(): void {
-		this.client.sendRequest("disconnect");
+		this.client.sendRequest('disconnect');
 	}
 }
--- Source/mongo/shell.ts
+++ Source/mongo/shell.ts
@@ -2,34 +2,26 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import * as cp from "child_process";
-import * as vscode from "vscode";
-import * as os from "os";
-import { IDisposable, toDisposable, dispose } from "./../util";
-import { EventEmitter, window } from "vscode";
+import * as cp from 'child_process';
+import * as vscode from 'vscode';
+import * as os from 'os';
+import { IDisposable, toDisposable, dispose } from './../util';
+import { EventEmitter, window } from 'vscode';
 
 export class Shell {
+
 	private executionId: number = 0;
 	private disposables: IDisposable[] = [];
 
-	private onResult: EventEmitter<{ exitCode; result; stderr }> =
-		new EventEmitter<{ exitCode; result; stderr }>();
+	private onResult: EventEmitter<{ exitCode, result, stderr }> = new EventEmitter<{ exitCode, result, stderr }>();
 
-	public static create(
-		execPath: string,
-		connectionString: string
-	): Promise<Shell> {
+	public static create(execPath: string, connectionString: string): Promise<Shell> {
 		return new Promise((c, e) => {
 			try {
-				const shellProcess = cp.spawn(execPath, [
-					"--quiet",
-					connectionString,
-				]);
+				const shellProcess = cp.spawn(execPath, ['--quiet', connectionString]);
 				return c(new Shell(shellProcess));
 			} catch (error) {
-				e(
-					`Error while creating mongo shell with path ${execPath}: ${error}`
-				);
+				e(`Error while creating mongo shell with path ${execPath}: ${error}`);
 			}
 		});
 	}
@@ -41,46 +33,36 @@ export class Shell {
 	private initialize() {
 		const once = (ee: NodeJS.EventEmitter, name: string, fn: Function) => {
 			ee.once(name, fn);
-			this.disposables.push(
-				toDisposable(() => ee.removeListener(name, fn))
-			);
+			this.disposables.push(toDisposable(() => ee.removeListener(name, fn)));
 		};
 
 		const on = (ee: NodeJS.EventEmitter, name: string, fn: Function) => {
 			ee.on(name, fn);
-			this.disposables.push(
-				toDisposable(() => ee.removeListener(name, fn))
-			);
+			this.disposables.push(toDisposable(() => ee.removeListener(name, fn)));
 		};
 
-		once(this.mongoShell, "error", (result) => this.onResult.fire(result));
-		once(this.mongoShell, "exit", (result) => this.onResult.fire(result));
+		once(this.mongoShell, 'error', result => this.onResult.fire(result));
+		once(this.mongoShell, 'exit', result => this.onResult.fire(result));
 
 		let buffers: string[] = [];
-		on(this.mongoShell.stdout, "data", (b) => {
-			let data: string = b.toString();
+		on(this.mongoShell.stdout, 'data', b => {
+			let data: string = b.toString()
 			const delimitter = `${this.executionId}${os.EOL}`;
 			if (data.endsWith(delimitter)) {
-				const result =
-					buffers.join("") +
-					data.substring(0, data.length - delimitter.length);
+				const result = buffers.join('') + data.substring(0, data.length - delimitter.length);
 				buffers = [];
 				this.onResult.fire({
 					exitCode: void 0,
 					result,
-					stderr: void 0,
+					stderr: void 0
 				});
 			} else {
 				buffers.push(b);
 			}
 		});
 
-		on(this.mongoShell.stderr, "data", (result) =>
-			this.onResult.fire(result)
-		);
-		once(this.mongoShell.stderr, "close", (result) =>
-			this.onResult.fire(result)
-		);
+		on(this.mongoShell.stderr, 'data', result => this.onResult.fire(result));
+		once(this.mongoShell.stderr, 'close', result => this.onResult.fire(result));
 	}
 
 	async useDatabase(databse: string): Promise<string> {
@@ -92,9 +74,9 @@ export class Shell {
 		const executionId = this._generateExecutionSequenceId();
 
 		try {
-			this.mongoShell.stdin.write(script, "utf8");
+			this.mongoShell.stdin.write(script, 'utf8');
 			this.mongoShell.stdin.write(os.EOL);
-			this.mongoShell.stdin.write(executionId, "utf8");
+			this.mongoShell.stdin.write(executionId, 'utf8');
 			this.mongoShell.stdin.write(os.EOL);
 		} catch (error) {
 			window.showErrorMessage(error.toString());
@@ -110,18 +92,13 @@ export class Shell {
 			let executed = false;
 			const handler = setTimeout(() => {
 				if (!executed) {
-					e("Timed out executing " + script);
+					e('Timed out executing ' + script);
 				}
 			}, 5000);
-			const disposable = this.onResult.event((result) => {
+			const disposable = this.onResult.event(result => {
 				disposable.dispose();
-				let lines = (<string>result.result)
-					.split(os.EOL)
-					.filter((line) => !!line && line !== 'Type "it" for more');
-				lines =
-					lines[lines.length - 1] === 'Type "it" for more'
-						? lines.splice(lines.length - 1, 1)
-						: lines;
+				let lines = (<string>result.result).split(os.EOL).filter(line => !!line && line !== 'Type "it" for more');
+				lines = lines[lines.length - 1] === 'Type "it" for more' ? lines.splice(lines.length - 1, 1) : lines;
 				let value = lines.join(os.EOL);
 				executed = true;
 				c(lines.join(os.EOL));
@@ -133,11 +110,11 @@ export class Shell {
 	}
 
 	private convertToSingleLine(script: string): string {
-		return script
-			.split(os.EOL)
-			.map((line) => line.trim())
-			.join("")
+		return script.split(os.EOL)
+			.map(line => line.trim())
+			.join('')
 			.trim();
+
 	}
 
 	private _generateExecutionSequenceId(): string {
